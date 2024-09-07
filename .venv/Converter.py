a945efd04ae6b4363c106dd8c887980e932a6633
Anuar <kenzhibekovanuar@gmail.com>
2024-09-07 13:44:47 +0500

    Initial commit

---
+++ .venv/Converter.py
@@ -0,0 +1,52 @@
+from tkinter import filedialog
+from tkinter import ttk
+import moviepy.editor
+from pathlib import Path
+from tkinter import *
+import tkinter as tk
+import time
+import threading
+
+
+root = Tk()
+
+root['bg'] = 'white'
+root.title('Converter')
+root.geometry('500x500')
+root.resizable(False, False)
+
+pb = ttk.Progressbar(root, orient='horizontal', mode='determinate', length=200)
+
+message_label = tk.Label(root, text="", bg='white',fg='green', font=("Arial", 12))
+message_label.pack(pady=10)
+
+def add_file():
+    file_path = filedialog.askopenfilename()
+    if file_path:
+
+        video_file = Path(file_path)
+        print(f"Selected file: {file_path}")
+        threading.Thread(target=convert_to_audio, args=(video_file,)).start()
+        convert_to_audio(video_file)
+
+
+        pb.pack(pady=10)
+        pb['value'] = 0
+
+def convert_to_audio(video_file):
+    video = moviepy.editor.VideoFileClip(f'{video_file}')
+    audio = video.audio
+    desktop_path = Path.home() / 'Desktop'
+    output_path = desktop_path / f'{video_file.stem}.mp3'
+
+    audio.write_audiofile(output_path)
+
+    pb['value'] = 100
+
+    message_label.config(text=f"Audio saved to {output_path}", fg = 'green')
+
+
+add_file_button = tk.Button(root, text="Add file", command=add_file)
+add_file_button.pack(pady=20)
+
+root.mainloop()
